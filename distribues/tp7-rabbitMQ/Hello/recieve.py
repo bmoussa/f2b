dfb40e2cdb7e62b020487d5febcdeac9f5118529
caio valente <caio-valente@hotmail.com>
2017-10-25 11:29:37 +0200

    distribues tp7 changes

--- distribues/tp7-rabbitMQ/Hello/recieve.py
+++ distribues/tp7-rabbitMQ/Hello/recieve.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % (body,))
+	
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+print(connection)
 channel = connection.channel()
-
+print(channel)
 channel.queue_declare(queue='hello')
 
-print ' [*] Waiting for messages. To exit press CTRL+C'
+print(' [*] Waiting for messages. To exit press CTRL+C')
 
-def callback(ch, method, properties, body):
-    print " [x] Received %r" % (body,)
-
-channel.basic_consume(callback,
-                      queue='hello',
-                      no_ack=True)
+channel.basic_consume(callback, queue='hello', no_ack=True)
 
 channel.start_consuming()
--- distribues/tp7-rabbitMQ/WorkQueues/worker.py
+++ distribues/tp7-rabbitMQ/WorkQueues/worker.py
@@ -7,15 +7,17 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(
 channel = connection.channel()
 
 channel.queue_declare(queue='task_queue', durable=True)
-print ' [*] Waiting for messages. To exit press CTRL+C'
+print(' [*] Waiting for messages. To exit press CTRL+C')
 
 def callback(ch, method, properties, body):
-    print " [x] Received %r" % (body,)
-    time.sleep( body.count('.') )
-    print " [x] Done"
+    print(" [x] Received %r" % body)
+    time.sleep(body.count(b'.'))
+    print(" [x] Done")
     ch.basic_ack(delivery_tag = method.delivery_tag)
 
+# to distributed better the task between many workers
 channel.basic_qos(prefetch_count=1)
+
 channel.basic_consume(callback,
                       queue='task_queue')
 
