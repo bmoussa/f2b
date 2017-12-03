6e64f03c9da79af462136d6b040592c1133ecaf0
caio valente <caio-valente@hotmail.com>
2017-12-03 18:57:48 +0100

    [.net] [tp2] dll linkeds, able to launch both thread types

--- dotnet/tp2_threads/Premier/Program.cs
+++ dotnet/tp2_threads/Premier/Program.cs
@@ -1,24 +1,22 @@
-﻿using System;
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading;
 
 namespace Premier
 {
-    class NombrePremier
+    public class NombrePremier
     {
-        [STAThread]
-        static void Main(string[] args)
+        public NombrePremier()
         {
-            // L'instence de ThreadStart demande un delegate en paramètre ce qui est plus
-            // ou moins (mais pas vraiment) l'équivalent d'un pointeur de fonction
-
+            CreateConsole();
             Console.BackgroundColor = ConsoleColor.Blue;
-            Console.SetWindowSize(7, 20);  // la taille de la fenêtre
-           
-            Thread t = new Thread(new ThreadStart(ThreadFunction));
-            t.Start();
+            //Console.SetWindowSize(7, 20);  // la taille de la fenêtre
         }
 
-        private static void ThreadFunction()
+        public void CalculatePrimes()
         {
             for (int p = 1; p < 1000000; p++)
             {
@@ -30,8 +28,52 @@ namespace Premier
                 if (i == p)
                     Console.WriteLine(p.ToString());
                 Thread.Sleep(50);
-
             }
+        }
+
+        /*
+         * Following code creates a new console and redirects outputs to it
+         * 
+         * Solution found at:
+         * https://stackoverflow.com/questions/41624103/console-out-output-is-showing-in-output-window-needed-in-allocconsole 
+         *
+         */
+
+        [DllImport("kernel32.dll", EntryPoint = "AllocConsole", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        private static extern int AllocConsole();
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr CreateFile(
+            string lpFileName,
+            uint dwDesiredAccess,
+            uint dwShareMode,
+            uint lpSecurityAttributes,
+            uint dwCreationDisposition,
+            uint dwFlagsAndAttributes,
+            uint hTemplateFile);
+
+        private const int MY_CODE_PAGE = 437;
+        private const uint GENERIC_WRITE = 0x40000000;
+        private const uint FILE_SHARE_WRITE = 0x2;
+        private const uint OPEN_EXISTING = 0x3;
+
+        public static void CreateConsole()
+        {
+            AllocConsole();
+
+            IntPtr stdHandle = CreateFile(
+                "CONOUT$",
+                GENERIC_WRITE,
+                FILE_SHARE_WRITE,
+                0, OPEN_EXISTING, 0, 0
+            );
+
+            SafeFileHandle safeFileHandle = new SafeFileHandle(stdHandle, true);
+            FileStream fileStream = new FileStream(safeFileHandle, FileAccess.Write);
+            Encoding encoding = System.Text.Encoding.GetEncoding(MY_CODE_PAGE);
+            StreamWriter standardOutput = new StreamWriter(fileStream, encoding);
+            standardOutput.AutoFlush = true;
+            Console.SetOut(standardOutput);
         }
     }
 }
--- dotnet/tp2_threads/tp2_thread/FormProcessManager.cs
+++ dotnet/tp2_threads/tp2_thread/FormProcessManager.cs
@@ -6,9 +6,12 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Premier;
+using WpfAppliTh;
+using System.Windows;
 
 namespace tp1_process
 {
@@ -17,16 +20,15 @@ namespace tp1_process
         /* --------------- CONSTANTS ---------------- */
         static string BALL_DLL_NAME = "Ballon";
         static string PRIME_DLL_NAME = "Premier";
-        static string DLL_EXTENSION = ".dll";
 
 
         /* --------------- GLOBAL VARIABLES ---------------- */
-        // list of child processes
-        private List<Process> procs = new List<Process>();
-        // state of processes table
-        private bool show_procs = true;
-        // delegate used by Asynchronous tasks that may return Process objects
-        delegate void ProcessArgReturningVoidDelegate(Process p);
+        // list of child threads
+        private List<Thread> threads = new List<Thread>();
+        // state of threads table
+        private bool show_threads = true;
+        // delegate used by Asynchronous tasks that may return Thread objects
+        delegate void ThreadArgReturningVoidDelegate(Thread p);
 
 
         public process_manager()
@@ -52,7 +54,7 @@ namespace tp1_process
             // finish child processes before exiting
             finish_childs();
         }
-        
+
         // callback for menu => Quit
         private void quitToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -63,68 +65,68 @@ namespace tp1_process
             Close();
         }
 
-        // callback for menu => Create => Ball Process
+        // callback for menu => Create => Ball Thread
         private void ballProcessToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            create_process(BALL_DLL_NAME + DLL_EXTENSION);
+            launch_thread(BALL_DLL_NAME);
         }
-        
-        // callback for menu => Create => Prime Process
+
+        // callback for menu => Create => Prime Thread
         private void primeProcessToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            create_process(PRIME_DLL_NAME + DLL_EXTENSION);
+            launch_thread(PRIME_DLL_NAME);
         }
-        
+
         // callback for the X button of a child
         void child_process_exited(object sender, EventArgs e)
         {
             // process must do this call using Invoke because it's another thread
-            void remove_exited_child(Process p)
+            void remove_exited_child(Thread p)
             {
-                this.procs.Remove(p);
+                this.threads.Remove(p);
                 this.update_listView();
             }
 
             if (this.listView1.InvokeRequired)
             {
-                Process p = (Process)sender;
-                ProcessArgReturningVoidDelegate d = new ProcessArgReturningVoidDelegate(remove_exited_child);
+                Thread p = (Thread)sender;
+                ThreadArgReturningVoidDelegate d = new ThreadArgReturningVoidDelegate(remove_exited_child);
                 this.Invoke(d, new object[] { p });
             }
         }
-        
+
         // callback for menu => Show/Hide table
         private void showProcessToolStripMenuItem_Click(object sender, EventArgs e)
         {
             // invert table status
-            show_procs = !show_procs;
+            show_threads = !show_threads;
 
             // update table according to new state
-            if (show_procs)
+            if (show_threads)
                 this.listView1.Show();
             else
                 this.listView1.Hide();
         }
 
-        // callback for menu => Delete => Last Ball Process
+        // callback for menu => Delete => Last Ball Thread
         private void lastBallProcessToolStripMenuItem_Click(object sender, EventArgs e)
         {
             finish_last_process_with_name(BALL_DLL_NAME);
         }
 
-        // callback for menu => Delete => Last Prime Process
+        // callback for menu => Delete => Last Prime Thread
         private void lastPrimeProcessToolStripMenuItem_Click(object sender, EventArgs e)
         {
             finish_last_process_with_name(PRIME_DLL_NAME);
         }
 
-        // callback for menu => Delete => Last Process
+        // callback for menu => Delete => Last Thread
         private void lastProcessToolStripMenuItem_Click(object sender, EventArgs e)
         {
             finish_last();
         }
 
-        // callback for menu => Delete => All Processes
+        // callback for menu => Delete => All Threades
         private void allProcessesToolStripMenuItem_Click(object sender, EventArgs e)
         {
             finish_childs();
@@ -133,53 +135,58 @@ namespace tp1_process
         }
 
         /* --------------- AUXILIARY FUNCTIONS ---------------- */
-
-        // create a process with name process_name, 
-        // append it to processes list and refresh table
-        private void create_process(string process_name)
-        {
-            // create the process with a callback function
-            Process p = Process.Start(process_name);
-            p.EnableRaisingEvents = true;
-            p.Exited += new EventHandler(child_process_exited);
-
-            // append process to list
-            procs.Add(p);
-
-            update_listView();
-        }
-
-        /*
+        
         // launch a thread with name thread_name, 
-        // append it to processes list and refresh table
+        // append it to threads list and refresh table
         private void launch_thread(string thread_name)
         {
-            ProcessThread
-            Process p = Process.Start(process_name);
-            p.EnableRaisingEvents = true;
-            p.Exited += new EventHandler(child_process_exited);
+            void launch_thread_ball()
+            {
+                WindowBallon b = new WindowBallon();
+                b.Show();
+                System.Windows.Threading.Dispatcher.Run();
+            }
+            void launch_thread_prime()
+            {
+                NombrePremier p = new NombrePremier();
+                p.CalculatePrimes();
+                System.Windows.Threading.Dispatcher.Run();
+            }
+
+            Thread t = (thread_name == BALL_DLL_NAME) ?
+                    new Thread(launch_thread_ball) :
+                    new Thread(launch_thread_prime);
+
+            t.Name = thread_name;
+
+            // thread must be STA to change UI components
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+
+            // TODO: add exit callback
+            //p.EnableRaisingEvents = true;
+            //p.Exited += new EventHandler(child_process_exited);
 
             // append process to list
-            procs.Add(p);
+            threads.Add(t);
 
             update_listView();
         }
-        */
 
         // finish the last launched process with name process_name, 
         // remove it from processes list and refresh table
         private void finish_last_process_with_name(string process_name)
         {
             // find last process with given name
-            bool hasName(Process p) => process_name.Equals(p.ProcessName);
-            Predicate<Process> hasProcName = hasName;
-            Process last = this.procs.FindLast(hasProcName);
+            bool hasName(Thread p) => process_name.Equals(p.Name);
+            Predicate<Thread> hasProcName = hasName;
+            Thread last = this.threads.FindLast(hasProcName);
 
             finish_process(last);
 
             // remove from global list
-            this.procs.Remove(last);
-            
+            this.threads.Remove(last);
+
             update_listView();
         }
 
@@ -188,22 +195,24 @@ namespace tp1_process
         private void finish_last()
         {
             // finish last process from list and remove from list
-            Process last = this.procs.Last();
+            Thread last = this.threads.Last();
             finish_process(last);
-            this.procs.Remove(last);
+            this.threads.Remove(last);
 
             update_listView();
         }
 
         // finish a process by releasing its resources and closing its window
-        private void finish_process(Process p)
+        private void finish_process(Thread p)
         {
             if (p != null)
             {
+                /*
                 // Close process by sending a close message to its main window.
                 p.CloseMainWindow();
                 // Free resources associated to process.
                 p.Close();
+                */
             }
         }
 
@@ -211,16 +220,16 @@ namespace tp1_process
         private void finish_childs()
         {
             // finish child processes
-            foreach (Process p in this.procs)
+            foreach (Thread p in this.threads)
             {
                 finish_process(p);
             }
 
-            // empty procs list
-            this.procs.Clear();
+            // empty threads list
+            this.threads.Clear();
         }
-        
-        // clean table and redraw it using the actual data of procs list
+
+        // clean table and redraw it using the actual data of threads list
         private void update_listView()
         {
             // empty listview
@@ -228,15 +237,15 @@ namespace tp1_process
 
             // draw listview headers
             this.listView1.View = View.Details;
-            this.listView1.Columns.Add("Index", 40, HorizontalAlignment.Left);
-            this.listView1.Columns.Add("Thread PID", 100, HorizontalAlignment.Left);
-            this.listView1.Columns.Add("Thread name", 100, HorizontalAlignment.Left);
+            this.listView1.Columns.Add("Index", 40, System.Windows.Forms.HorizontalAlignment.Left);
+            this.listView1.Columns.Add("Thread PID", 100, System.Windows.Forms.HorizontalAlignment.Left);
+            this.listView1.Columns.Add("Thread name", 100, System.Windows.Forms.HorizontalAlignment.Left);
 
-            // fill listview using procs list
+            // fill listview using threads list
             int i = 1;
-            foreach (Process p in this.procs)
+            foreach (Thread p in this.threads)
             {
-                string[] arr = {i++.ToString(), p.Id.ToString(), p.ProcessName};
+                string[] arr = { i++.ToString(), p.Priority.ToString(), p.Name };
                 ListViewItem itm = new ListViewItem(arr);
                 listView1.Items.Add(itm);
             }
@@ -244,6 +253,6 @@ namespace tp1_process
             // redraw updated listview
             listView1.Refresh();
         }
-        
+
     }
 }
