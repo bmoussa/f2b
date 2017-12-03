8609b0bbf33c3e317ce8de169d38ca66856cc6fe
caio valente <caio-valente@hotmail.com>
2017-12-03 12:22:17 +0100

    [.net] [tp2] add initial files (tp2_thread and Premier dll)

---
+++ dotnet/tp2_threads/tp2_thread/FormProcessManager.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace tp1_process
+{
+    partial class process_manager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.LaunchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ballProcessToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.primeProcessToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lastProcessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lastBallProcessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lastPrimeProcessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.allProcessesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showProcessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ballProcessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.primeProcessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.LaunchToolStripMenuItem,
+            this.deleteToolStripMenuItem,
+            this.showProcessToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(341, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // LaunchToolStripMenuItem
+            // 
+            this.LaunchToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ballProcessToolStripMenuItem1,
+            this.primeProcessToolStripMenuItem1});
+            this.LaunchToolStripMenuItem.Name = "LaunchToolStripMenuItem";
+            this.LaunchToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.LaunchToolStripMenuItem.Text = "Launch";
+            // 
+            // ballProcessToolStripMenuItem1
+            // 
+            this.ballProcessToolStripMenuItem1.Name = "ballProcessToolStripMenuItem1";
+            this.ballProcessToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.ballProcessToolStripMenuItem1.Text = "Ball thread";
+            this.ballProcessToolStripMenuItem1.Click += new System.EventHandler(this.ballProcessToolStripMenuItem1_Click);
+            // 
+            // primeProcessToolStripMenuItem1
+            // 
+            this.primeProcessToolStripMenuItem1.Name = "primeProcessToolStripMenuItem1";
+            this.primeProcessToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.primeProcessToolStripMenuItem1.Text = "Prime thread";
+            this.primeProcessToolStripMenuItem1.Click += new System.EventHandler(this.primeProcessToolStripMenuItem1_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lastProcessToolStripMenuItem,
+            this.lastBallProcessToolStripMenuItem,
+            this.lastPrimeProcessToolStripMenuItem,
+            this.allProcessesToolStripMenuItem});
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            // 
+            // lastProcessToolStripMenuItem
+            // 
+            this.lastProcessToolStripMenuItem.Name = "lastProcessToolStripMenuItem";
+            this.lastProcessToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.lastProcessToolStripMenuItem.Text = "Last thread";
+            this.lastProcessToolStripMenuItem.Click += new System.EventHandler(this.lastProcessToolStripMenuItem_Click);
+            // 
+            // lastBallProcessToolStripMenuItem
+            // 
+            this.lastBallProcessToolStripMenuItem.Name = "lastBallProcessToolStripMenuItem";
+            this.lastBallProcessToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.lastBallProcessToolStripMenuItem.Text = "Last ball thread";
+            this.lastBallProcessToolStripMenuItem.Click += new System.EventHandler(this.lastBallProcessToolStripMenuItem_Click);
+            // 
+            // lastPrimeProcessToolStripMenuItem
+            // 
+            this.lastPrimeProcessToolStripMenuItem.Name = "lastPrimeProcessToolStripMenuItem";
+            this.lastPrimeProcessToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.lastPrimeProcessToolStripMenuItem.Text = "Last prime thread";
+            this.lastPrimeProcessToolStripMenuItem.Click += new System.EventHandler(this.lastPrimeProcessToolStripMenuItem_Click);
+            // 
+            // allProcessesToolStripMenuItem
+            // 
+            this.allProcessesToolStripMenuItem.Name = "allProcessesToolStripMenuItem";
+            this.allProcessesToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.allProcessesToolStripMenuItem.Text = "All threads";
+            this.allProcessesToolStripMenuItem.Click += new System.EventHandler(this.allProcessesToolStripMenuItem_Click);
+            // 
+            // showProcessToolStripMenuItem
+            // 
+            this.showProcessToolStripMenuItem.Name = "showProcessToolStripMenuItem";
+            this.showProcessToolStripMenuItem.Size = new System.Drawing.Size(107, 20);
+            this.showProcessToolStripMenuItem.Text = "Show/Hide table";
+            this.showProcessToolStripMenuItem.Click += new System.EventHandler(this.showProcessToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(0, 27);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(341, 359);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // ballProcessToolStripMenuItem
+            // 
+            this.ballProcessToolStripMenuItem.Name = "ballProcessToolStripMenuItem";
+            this.ballProcessToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ballProcessToolStripMenuItem.Text = "Ball process";
+            // 
+            // primeProcessToolStripMenuItem
+            // 
+            this.primeProcessToolStripMenuItem.Name = "primeProcessToolStripMenuItem";
+            this.primeProcessToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.primeProcessToolStripMenuItem.Text = "Prime process";
+            // 
+            // process_manager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 377);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "process_manager";
+            this.Text = "Thread manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lastProcessToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lastBallProcessToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lastPrimeProcessToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem allProcessesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showProcessToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ToolStripMenuItem LaunchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ballProcessToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem primeProcessToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ballProcessToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem primeProcessToolStripMenuItem1;
+    }
+}
+
---
+++ dotnet/tp2_threads/tp2_thread/FormProcessManager.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace tp1_process
+{
+    public partial class process_manager : Form
+    {
+        /* --------------- CONSTANTS ---------------- */
+        static string BALL_DLL_NAME = "Ballon";
+        static string PRIME_DLL_NAME = "Premier";
+        static string DLL_EXTENSION = ".dll";
+
+
+        /* --------------- GLOBAL VARIABLES ---------------- */
+        // list of child processes
+        private List<Process> procs = new List<Process>();
+        // state of processes table
+        private bool show_procs = true;
+        // delegate used by Asynchronous tasks that may return Process objects
+        delegate void ProcessArgReturningVoidDelegate(Process p);
+
+
+        public process_manager()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // add X button callback
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormClosingHandler);
+
+            // initialize listview
+            update_listView();
+        }
+
+
+        /* --------------- CALLBACK FUNCTIONS ---------------- */
+
+        // callback for X button (close)
+        private void FormClosingHandler(object sender, EventArgs e)
+        {
+            // finish child processes before exiting
+            finish_childs();
+        }
+        
+        // callback for menu => Quit
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // finish child processes before exiting
+            finish_childs();
+
+            // finish this process
+            Close();
+        }
+
+        // callback for menu => Create => Ball Process
+        private void ballProcessToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            create_process(BALL_DLL_NAME + DLL_EXTENSION);
+        }
+        
+        // callback for menu => Create => Prime Process
+        private void primeProcessToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            create_process(PRIME_DLL_NAME + DLL_EXTENSION);
+        }
+        
+        // callback for the X button of a child
+        void child_process_exited(object sender, EventArgs e)
+        {
+            // process must do this call using Invoke because it's another thread
+            void remove_exited_child(Process p)
+            {
+                this.procs.Remove(p);
+                this.update_listView();
+            }
+
+            if (this.listView1.InvokeRequired)
+            {
+                Process p = (Process)sender;
+                ProcessArgReturningVoidDelegate d = new ProcessArgReturningVoidDelegate(remove_exited_child);
+                this.Invoke(d, new object[] { p });
+            }
+        }
+        
+        // callback for menu => Show/Hide table
+        private void showProcessToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // invert table status
+            show_procs = !show_procs;
+
+            // update table according to new state
+            if (show_procs)
+                this.listView1.Show();
+            else
+                this.listView1.Hide();
+        }
+
+        // callback for menu => Delete => Last Ball Process
+        private void lastBallProcessToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            finish_last_process_with_name(BALL_DLL_NAME);
+        }
+
+        // callback for menu => Delete => Last Prime Process
+        private void lastPrimeProcessToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            finish_last_process_with_name(PRIME_DLL_NAME);
+        }
+
+        // callback for menu => Delete => Last Process
+        private void lastProcessToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            finish_last();
+        }
+
+        // callback for menu => Delete => All Processes
+        private void allProcessesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            finish_childs();
+
+            update_listView();
+        }
+
+        /* --------------- AUXILIARY FUNCTIONS ---------------- */
+
+        // create a process with name process_name, 
+        // append it to processes list and refresh table
+        private void create_process(string process_name)
+        {
+            // create the process with a callback function
+            Process p = Process.Start(process_name);
+            p.EnableRaisingEvents = true;
+            p.Exited += new EventHandler(child_process_exited);
+
+            // append process to list
+            procs.Add(p);
+
+            update_listView();
+        }
+
+        /*
+        // launch a thread with name thread_name, 
+        // append it to processes list and refresh table
+        private void launch_thread(string thread_name)
+        {
+            ProcessThread
+            Process p = Process.Start(process_name);
+            p.EnableRaisingEvents = true;
+            p.Exited += new EventHandler(child_process_exited);
+
+            // append process to list
+            procs.Add(p);
+
+            update_listView();
+        }
+        */
+
+        // finish the last launched process with name process_name, 
+        // remove it from processes list and refresh table
+        private void finish_last_process_with_name(string process_name)
+        {
+            // find last process with given name
+            bool hasName(Process p) => process_name.Equals(p.ProcessName);
+            Predicate<Process> hasProcName = hasName;
+            Process last = this.procs.FindLast(hasProcName);
+
+            finish_process(last);
+
+            // remove from global list
+            this.procs.Remove(last);
+            
+            update_listView();
+        }
+
+        // finish the last launched process, 
+        // remove it from processes list and refresh table
+        private void finish_last()
+        {
+            // finish last process from list and remove from list
+            Process last = this.procs.Last();
+            finish_process(last);
+            this.procs.Remove(last);
+
+            update_listView();
+        }
+
+        // finish a process by releasing its resources and closing its window
+        private void finish_process(Process p)
+        {
+            if (p != null)
+            {
+                // Close process by sending a close message to its main window.
+                p.CloseMainWindow();
+                // Free resources associated to process.
+                p.Close();
+            }
+        }
+
+        // finish all childs of the manager process
+        private void finish_childs()
+        {
+            // finish child processes
+            foreach (Process p in this.procs)
+            {
+                finish_process(p);
+            }
+
+            // empty procs list
+            this.procs.Clear();
+        }
+        
+        // clean table and redraw it using the actual data of procs list
+        private void update_listView()
+        {
+            // empty listview
+            this.listView1.Clear();
+
+            // draw listview headers
+            this.listView1.View = View.Details;
+            this.listView1.Columns.Add("Index", 40, HorizontalAlignment.Left);
+            this.listView1.Columns.Add("Thread PID", 100, HorizontalAlignment.Left);
+            this.listView1.Columns.Add("Thread name", 100, HorizontalAlignment.Left);
+
+            // fill listview using procs list
+            int i = 1;
+            foreach (Process p in this.procs)
+            {
+                string[] arr = {i++.ToString(), p.Id.ToString(), p.ProcessName};
+                ListViewItem itm = new ListViewItem(arr);
+                listView1.Items.Add(itm);
+            }
+
+            // redraw updated listview
+            listView1.Refresh();
+        }
+        
+    }
+}
