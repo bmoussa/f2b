39c4f8737bdc6b36c14f5886df1a75010a93f5ba
caio valente <caio-valente@hotmail.com>
2017-12-05 17:20:47 +0100

    [.net] [tp2] add project balloon

---
+++ dotnet/tp2_threads/WpfCustomControlLibrary_balloon/WpfBallon.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+
+namespace WpfAppliTh
+{
+    /// <summary>
+    /// Interaction logic for WindowBallon.xaml
+    /// </summary>
+    public partial class WindowBallon : Window
+    {
+        public WindowBallon()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult key = MessageBox.Show(
+               "Etes-vous sûre de vouloir quitter",
+               "Confirmez",
+               MessageBoxButton.YesNo,
+                MessageBoxImage.Question,
+                MessageBoxResult.No);
+            if (!(e.Cancel = (key == MessageBoxResult.No)))
+            {
+                e.Cancel = false;
+            }
+        }
+    }
+}
